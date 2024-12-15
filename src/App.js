7cf4b68eb030743b46c0c9ed9175708986dec576
Dennis Stagliano <das7701@gmail.com>
2024-12-15 17:41:16 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1); // on reload always starts at 0
+  }
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice
+      </p>
+    </div>
+  );
+}
